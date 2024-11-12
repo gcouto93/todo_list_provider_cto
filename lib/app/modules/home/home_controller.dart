e3b9a5e32d83336667d184c8f38254252403c964
Gabriel <gcouto93@gmail.com>
2024-11-12 14:39:45 -0300

    pequenos ajustes

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,11 +1,45 @@
 import 'package:cto_todo_list_provider/app/core/notifier/default_change_notifier.dart';
 import 'package:cto_todo_list_provider/app/models/task_filter_enum.dart';
+import 'package:cto_todo_list_provider/app/models/task_model.dart';
+import 'package:cto_todo_list_provider/app/models/total_tasks_model.dart';
+import 'package:cto_todo_list_provider/app/models/week_task_model.dart';
+import 'package:cto_todo_list_provider/app/services/tasks/tasks_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeController extends DefaultChangeNotifier {
 
+final TasksService _tasksService;
 var filterSelected = TaskFilterEnum.today;
+TotalTasksModel? todayTotalTasks;
+TotalTasksModel? tomorrowTotalTasks;
+TotalTasksModel? weekTotalTasks;
 
+HomeController({required TasksService taskService}) : _tasksService = taskService;
+
+
+
+Future<void> loadTotalTasks() async {
+  final allTasks = await Future.wait([
+  _tasksService.getToday(),
+  _tasksService.getTomorrow(),
+  _tasksService.getWeek()
+  ]);
+
+  final toDayTasks = allTasks[0] as List<TaskModel>;
+  final tomorrowTasks = allTasks[1] as List<TaskModel>;
+  final weekTasks = allTasks[3] as WeekTaskModel;
+
+  todayTotalTasks = TotalTasksModel(
+    totalTasks: toDayTasks.length,
+     totalTasksFinish: toDayTasks.where((task) => task.finish).length);
+  tomorrowTotalTasks = TotalTasksModel(
+    totalTasks: tomorrowTasks.length,
+     totalTasksFinish: tomorrowTasks.where((task) => task.finish).length);
+  todayTotalTasks = TotalTasksModel(
+    totalTasks: weekTasks.tasks.length,
+     totalTasksFinish: weekTasks.tasks.where((task) => task.finish).length);
+  notifyListeners();
+}
 
 
 }
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,11 @@
 import 'package:cto_todo_list_provider/app/core/modules/todo_list_module.dart';
 import 'package:cto_todo_list_provider/app/modules/home/home_controller.dart';
 import 'package:cto_todo_list_provider/app/modules/home/home_page.dart';
+import 'package:cto_todo_list_provider/app/modules/tasks/task_create_controller.dart';
+import 'package:cto_todo_list_provider/app/repositories/tasks/task_repository.dart';
+import 'package:cto_todo_list_provider/app/repositories/tasks/task_repository_impl.dart';
+import 'package:cto_todo_list_provider/app/services/tasks/task_service_impl.dart';
+import 'package:cto_todo_list_provider/app/services/tasks/tasks_service.dart';
 import 'package:provider/provider.dart';
 
 class HomeModule extends TodoListModule{
@@ -10,7 +15,20 @@ class HomeModule extends TodoListModule{
       '/home' : (context) => HomePage(),
     },
      bindings:[
-      ChangeNotifierProvider(create: (context) => HomeController())
+      Provider<TaskRepository>(
+        create: (context) =>
+           TaskRepositoryImpl(sqliteConnectionFactory: context.read())
+      ),
+      Provider<TasksService>(
+        create: (context) =>
+           TaskServiceImpl(taskRepository: context.read())
+      ),
+
+      ChangeNotifierProvider(
+        create: (context) =>
+           TaskCreateController(taskService: context.read())
+      ),
+      ChangeNotifierProvider(create: (context) => HomeController(taskService: context.read()))
      ]
      );
 }
